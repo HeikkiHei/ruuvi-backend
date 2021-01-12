88a1e09ec8da3b8b0a2ed8f44d56b96707f3abef
Heikki Ahonen <heikki.ahonen@centria.fi>
2021-01-12 09:38:43 +0200

    Update

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+app.get('/', (request, response) => {
+  response.send('<h>Try /api/ruuvis instead</h>')
+})
+
 app.get('/api/ruuvis', (request, response) => {
   Ruuvi.find({}).then(ruuvis => {
     response.json(ruuvis.map(ruuvi => ruuvi.toJSON()))
@@ -17,7 +21,7 @@ app.get('/api/ruuvis', (request, response) => {
 
 app.post('/api/ruuvis', (request, response) => {
   const body = request.body
-  console.log('THIS IS BODY', body)
+  console.log('Sending this body:\n', body)
   if (body.tags === undefined) {
     return response.status(400).json({ error: 'tags missing' })
   }
@@ -78,46 +82,6 @@ app.delete('/api/ruuvis/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put('/api/ruuvis/:id', (request, response, next) => {
-  const body = request.body
-
-  const ruuvi = {
-    tags: [
-      {
-        accelX: body.tags[0].accelX,
-        accelY: body.tags[0].accelY,
-        accelZ: body.tags[0].accelZ,
-        connectable: body.tags[0].connectable,
-        createDate: body.tags[0].createDate,
-        dataFormat: body.tags[0].dataFormat,
-        defaultBackground: body.tags[0].defaultBackground,
-        favorite: body.tags[0].favorite,
-        humidity: body.tags[0].humidity,
-        humidityOffset: body.tags[0].humidityOffset,
-        id: body.tags[0].id,
-        measurementSequenceNumber: body.tags[0].measurementSequenceNumber,
-        movementCounter: body.tags[0].movementCounter,
-        name: body.tags[0].name,
-        pressure: body.tags[0].pressure,
-        rssi: body.tags[0].rssi,
-        temperature: body.tags[0].temperature,
-        txPower: body.tags[0].txPower,
-        updateAt: body.tags[0].updateAt,
-        voltage: body.tags[0].voltage
-      }
-    ],
-    batteryLevel: body.batteryLevel,
-    deviceId: body.deviceId,
-    eventId: body.eventId,
-    time: body.time
-  }
-
-  Ruuvi.findByIdAndUpdate(request.params.id, ruuvi, { new: true })
-    .then(updatedRuuvi => {
-      response.json(updatedRuuvi.toJSON())
-    })
-    .catch(error => next(error))
-})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
